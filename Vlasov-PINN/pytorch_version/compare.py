fe1571f40bfcf993c92d320b3e8f91302d3f7dfd
zbyandmoon <41515753+zbyandmoon@users.noreply.github.com>
2022-07-14 19:23:27 +0800

    Delete Vlasov-PINN/pytorch_version directory

--- Vlasov-PINN/pytorch_version/compare.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-train_loss = np.genfromtxt('./result/train_loss.txt')
-lbfgs_loss = np.genfromtxt('./result/lbfgs_loss.txt')
-loss = np.concatenate((train_loss, lbfgs_loss))
-plt.plot(np.log10(loss))
-plt.savefig('loss.png')
-plt.show()
